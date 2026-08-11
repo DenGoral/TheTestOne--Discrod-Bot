c32ff6a18be101ca5a16feaaa83755ff75865570
Astroth <denys.goral@gmail.com>
2026-08-11 22:05:21 +0300

    again. random start uop comits

--- DiscordBot/obj/Debug/net9.0/DiscordBot.AssemblyInfo.cs
+++ DiscordBot/obj/Debug/net9.0/DiscordBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b87129043364dcdfcbf53cbdce498f907e7f1de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf83197f41d35bbc81ecd4f6f4b41154743fd3e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
