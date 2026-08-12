5acffd9f14de3150d632598fde0d77581ddadae7
Astroth <denys.goral@gmail.com>
2026-08-12 19:53:13 +0300

    added new command "!aboutme"

--- DiscordBot/Commands.cs
+++ DiscordBot/Commands.cs
@@ -15,6 +15,7 @@ public class Commands
         ["!list"] =  CommandList,
         ["!roll"] = Roll,
         ["!whenupd"] = WhenUpdate,
+        ["!aboutme"] = AboutMe,
         ["!giverole"] = async (msg) =>
         {
             if (msg.Author is SocketGuildUser user)
@@ -32,7 +33,8 @@ public class Commands
                        ```Here is the list of all available commands:
                        1. !roll - bot will pick random number between 1 and 100.
                        2. !list - bot will send list of available all commands.
-                       3. !hello - bot will respond with 'Hello! @<Username>'. ```
+                       3. !hello - bot will respond with 'Hello! @<Username>'. 
+                       4. !aboutme - bot will tell who he is!```
                        """;
         
         await message.Channel.SendMessageAsync(list);
@@ -49,6 +51,17 @@ public class Commands
     public static async Task Roll(SocketMessage message)
     {
         await message.Channel.SendMessageAsync($"Random number: {_randomNumber.Next(1, 100)}");
+    }
+
+    public static async Task AboutMe(SocketMessage message)
+    {
+        var about = $"""
+                       ```Hi! I am a bot developed by an amazing, charismatic, awesome, smart, humble, nonchalant 'ImAstroth'.
+                       I am the first ever bot developed by him, the programming genuis, i can do almost nothing, but you can suggest what can i add
+                       in the general channel! That is it! im going to kill you lol, get good nerd```
+                       """;
+
+        await message.Channel.SendMessageAsync(about);
     }
 
     public static async Task WelcomeMessage(SocketGuildUser user)
--- DiscordBot/obj/Debug/net9.0/DiscordBot.AssemblyInfo.cs
+++ DiscordBot/obj/Debug/net9.0/DiscordBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf83197f41d35bbc81ecd4f6f4b41154743fd3e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32ff6a18be101ca5a16feaaa83755ff75865570")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
